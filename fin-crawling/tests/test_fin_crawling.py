09feb106082ccb2bd718af541dcaeb4810c44d5c
tldrd0117 <tldrd0117@gmail.com>
2021-01-03 21:47:15 +0900

    initial commit

---
+++ fin-crawling/tests/test_fin_crawling.py
@@ -0,0 +1,5 @@
+from fin_crawling import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
---
+++ flask-api/README.md
@@ -0,0 +1,3 @@
+### commands
+- docker build -t flask-api .
+- docker run -p 5000:3031 flask-api
