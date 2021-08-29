e361aad57f297b190fbbace3fa073850e016dba5
tldrd0117 <tldrd0117@gmail.com>
2021-08-29 23:11:39 +0900

    test4

--- fin-crawling-server/server/app/crawler/MarcapCrawler.py
+++ fin-crawling-server/server/app/crawler/MarcapCrawler.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from watchdog.events import FileCreatedEvent
 import uuid
 
@@ -45,6 +46,15 @@ class MarcapCrawler(object):
         return str(uuid.uuid4())
 
     def connectWebDriver(self, addr: str, uuid: str) -> WebDriver:
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("window-size=1400,1500")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("start-maximized")
+        options.add_argument("enable-automation")
+        options.add_argument("--disable-infobars")
+        options.add_argument("--disable-dev-shm-usage")
         chrome_options = webdriver.ChromeOptions()
         prefs = {
             "download.prompt_for_download": False,
