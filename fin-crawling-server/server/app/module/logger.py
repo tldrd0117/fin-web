c7d55692d4c7452fcf9f8167623f6a878bda45cd
root <root@DESKTOP-S923CM3.localdomain>
2021-11-09 00:00:57 +0900

    add factors and logger

--- fin-crawling-server/server/app/module/logger.py
+++ fin-crawling-server/server/app/module/logger.py
@@ -3,13 +3,15 @@ import logging
 import logging.handlers
 from app.util.DateUtils import getNowDateStr
 import pathlib
+from uvicorn.config import logger
 
 
 class Logger:
     def __init__(self, cls: str) -> None:
-        self.logger = logging.getLogger("logger")
+        self.logger = logging.getLogger("logger"+cls)
         self.cls = cls
         path = pathlib.Path("../../../log")
+        logger.info(path.resolve())
 
         self.fileHandler = logging.handlers.TimedRotatingFileHandler(
             filename=path.resolve(),
